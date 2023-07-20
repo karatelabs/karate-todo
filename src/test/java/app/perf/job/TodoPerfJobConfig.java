00deba89a0f63634596409a5e4992959a9cd30f0
Peter Thomas <ptrthomas@gmail.com>
2023-07-20 15:55:57 +0530

    tweak mvn command for perf test

--- src/test/java/app/perf/job/TodoPerfJobConfig.java
+++ src/test/java/app/perf/job/TodoPerfJobConfig.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 public class TodoPerfJobConfig extends JobConfigBase<Integer> {
 
-    String mainCommand = "mvn -P gatling test-compile gatling:test -Dgatling.simulationClass=app.perf.job.TodoLongSimulation";
+    String mainCommand = "./mvnw -P gatling test-compile gatling:test -Dgatling.simulationClass=app.perf.job.TodoLongSimulation";
     String buildDir = FileUtils.getBuildDir();
     String reportDir = Constants.KARATE_REPORTS;
     String executorDir = buildDir + File.separator + "gatling";
@@ -99,15 +99,18 @@ public class TodoPerfJobConfig extends JobConfigBase<Integer> {
             "-Dexec.mainClass=io.gatling.app.Gatling", "-Dexec.args=-ro " + reportDir + " -rf " + buildDir});
     }
 
+    // ./mvnw exec:java -Dexec.mainClass=app.App -Dexec.args=classpath:app
+    // export URL_BASE=http://host.docker.internal:8080
     // mvn test-compile exec:java -Dexec.classpathScope=test -Dexec.mainClass=app.perf.job.TodoPerfJobConfig -Dkarate.env=perf -Dexec.args=5
-    // docker run -it --rm -v "$HOME/.m2":/root/.m2 karate-mvn java -jar karate.jar -j http://host.docker.internal:8090
+    // docker run -it --rm -v "$HOME/.m2":/root/.m2 karate-mvn java -jar karate.jar -j http://host.docker.internal:8090   
+    // ./mvnw exec:java -Dexec.classpathScope=test -Dexec.mainClass=com.intuit.karate.Main -Dexec.args="-j http://localhost:8090"
     public static void main(String[] args) {
         int count = 5;
         if (args.length > 0) {
             try {
                 count = Integer.valueOf(args[0]);
             } catch (Exception e) {
-
+                // ignore
             }
         }
         System.out.println("using worker node count: " + count);
