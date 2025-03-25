f58e1a02562ca6e57dbd3550d91c631c1559826f
Peter Thomas <ptrthomas@gmail.com>
2025-03-25 15:01:35 +0530

    added example of decoding brotli support

--- src/test/java/app/api/interop/Helper.java
+++ src/test/java/app/api/interop/Helper.java
@@ -1,5 +1,11 @@
 package app.api.interop;
 
+import com.aayushatharva.brotli4j.Brotli4jLoader;
+import com.aayushatharva.brotli4j.decoder.BrotliInputStream;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,6 +15,20 @@ public class Helper {
         Map<String, Object> map = new HashMap<>();
         map.put("message", arg);
         return map;
+    }
+
+    public static String fromBrotli(byte[] bytes) throws Exception {
+        Brotli4jLoader.ensureAvailability();
+        ByteArrayInputStream compressedStream = new ByteArrayInputStream(bytes);
+        BrotliInputStream decodingStream = new BrotliInputStream(compressedStream);
+        ByteArrayOutputStream decompressedStream = new ByteArrayOutputStream();
+        byte[] buffer = new byte[4096];
+        int bytesRead;
+        while ((bytesRead = decodingStream.read(buffer)) != -1) {
+            decompressedStream.write(buffer, 0, bytesRead);
+        }
+        decodingStream.close();
+        return decompressedStream.toString(StandardCharsets.UTF_8);
     }
 
 }
--- src/test/java/app/api/interop/interop.feature
+++ src/test/java/app/api/interop/interop.feature
@@ -1,6 +1,14 @@
 Feature:
 
-  Scenario:
+  Scenario: calling static method
     * def helper = Java.type('app.api.interop.Helper')
     * def result = helper.doWork('hello world')
     * match result == { "message": "hello world" }
+
+  Scenario:
+    * def helper = Java.type('app.api.interop.Helper')
+    * def encoded = karate.readAsBytes('encoded.bin')
+    * def decoded = helper.fromBrotli(encoded)
+    * match decoded == '{ "hello": "world" }'
+    * def json = karate.fromString(decoded)
+    * match json == { hello: 'world' }
